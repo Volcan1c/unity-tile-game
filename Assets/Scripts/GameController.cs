75243e32daba6b2f7061d120b675cf4ba7051b9a
Zellisback <zellisback@gmail.com>
2020-04-13 16:23:02 +0300

    add enemy to game scene, implement turn system

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,11 +6,12 @@ public class GameController : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
     [SerializeField] TilemapController tilemapController;
-    [SerializeField] CharacterController characterController;
+    [SerializeField] CharacterController[] characterControllers;
+    private int characterIndex;
 
     void Start()
     {
-        
+        characterIndex = 0;
     }
 
     void Update()
@@ -18,9 +19,13 @@ public class GameController : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             Vector3Int mousePosition = PositionToVector3Int(mainCamera.ScreenToWorldPoint(Input.mousePosition));
-            var tileToMoveTo = tilemapController.GetTile(mousePosition);
-            
-            if (tileToMoveTo != null) characterController.Move(mousePosition);
+            var tileTypeToMoveTo = tilemapController.GetTile(mousePosition);
+
+            if (tileTypeToMoveTo != null)
+            {
+                characterControllers[characterIndex].Move(mousePosition);
+                characterIndex = characterIndex < characterControllers.Length - 1 ? characterIndex + 1 : 0;
+            }
         }
     }
 
